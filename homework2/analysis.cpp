65f3e3d30cec54805b9a96e40e2166ae87e4fa09
YJWSZhy <117193137+YJWSZhy@users.noreply.github.com>
2022-11-21 22:48:07 +0800

    修改负数部分排序

--- homework2/analysis.cpp
+++ homework2/analysis.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <string>
+#include <chrono>
 #include <iostream>
 #include "mergeSort.cpp"
 #include "quickSort.cpp"
@@ -29,11 +30,13 @@ finish, 0.1875ms
  */
 int main() {
     int len = 100;
-    int bound = 1000;
+    int bound = 100;
+    string ss;
 
-    string unHandleArr[1000] = {};
+    string unHandleArr[100] = {};
     for (int i = 0; i < len; i++) {
         string s;
+        string s1;
         if (rand() % 2 != 0) {
             s.push_back('-');
         }
@@ -41,12 +44,13 @@ int main() {
         for (int j = 0; j < bound; j++) {
             char temp = (rand() % 10 + '0');
             s.push_back(temp);
+            s1.push_back('0');
         }
         unHandleArr[i] = s;
+        ss = s1;
+
     }
-
-
-    string arr[1000] = {};
+    string arr[100] = {};
     for (int i = 0; i < len; i++) {
         arr[i] = unHandleArr[i];
     }
@@ -56,10 +60,19 @@ int main() {
     steady_clock::time_point startTime = steady_clock::now();
     // 归并排序
 
+    string arr1[100] = {};
     mergeSort(arr, 0, len - 1);
+    for (int i = len -1 , j = 0; i >= 0; i--) {
+        if (arr[i] < ss) {
+            arr1[j++] = arr[i];
+        }
+        else{
+            arr1[i] = arr[i];
+        }
+    }
     cout << "mergeSort: ";
 //    for (int i = 0; i < len; i++) {
-//        cout << arr[i] << " ";
+//        cout << arr1[i] << " ";
 //    }
     cout << endl;
     steady_clock::time_point finishTime = steady_clock::now();
@@ -74,8 +87,16 @@ int main() {
     startTime = steady_clock::now();
     quickSort(arr, 0, len - 1);
     cout << "quickSort: ";
+    for (int i = len -1 , j = 0; i >= 0; i--) {
+        if (arr[i] < ss) {
+            arr1[j++] = arr[i];
+        }
+        else{
+            arr1[i] = arr[i];
+        }
+    }
 //    for (int i = 0; i < len; i++) {
-//        cout << arr[i] << " ";
+//        cout << arr1[i] << " ";
 //    }
     cout << endl;
     finishTime = steady_clock::now();
@@ -85,8 +106,16 @@ int main() {
     startTime = steady_clock::now();
     vector<string> radixVector = radixSort(unHandleArr, len);
     cout << "radixSort: ";
+    for (int i = len -1 , j = 0; i >= 0; i--) {
+        if (arr[i] < ss) {
+            arr1[j++] = arr[i];
+        }
+        else{
+            arr1[i] = arr[i];
+        }
+    }
 //    for (int i = 0; i < len; i++) {
-//        cout << arr[i] << " ";
+//        cout << arr1[i] << " ";
 //    }
     cout << endl;
     finishTime = steady_clock::now();
@@ -100,8 +129,16 @@ int main() {
     startTime = steady_clock::now();
     selectSort(arr, len);
     cout << "selectSort: ";
+    for (int i = len -1 , j = 0; i >= 0; i--) {
+        if (arr[i] < ss) {
+            arr1[j++] = arr[i];
+        }
+        else{
+            arr1[i] = arr[i];
+        }
+    }
 //    for (int i = 0; i < len; i++) {
-//        cout << arr[i] << " ";
+//        cout << arr1[i] << " ";
 //    }
     cout << endl;
     finishTime = steady_clock::now();
@@ -115,8 +152,16 @@ int main() {
     startTime = steady_clock::now();
     shellSort(arr, len);
     cout << "shellSort: ";
+    for (int i = len -1 , j = 0; i >= 0; i--) {
+        if (arr[i] < ss) {
+            arr1[j++] = arr[i];
+        }
+        else{
+            arr1[i] = arr[i];
+        }
+    }
 //    for (int i = 0; i < len; i++) {
-//        cout << arr[i] << " ";
+//        cout << arr1[i] << " ";
 //    }
     cout << endl;
     finishTime = steady_clock::now();
